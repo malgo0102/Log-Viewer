965c9b2b6283e75bb1b38698aa2605fb7c51c944
Malgo0102 <mal.wit@hotmail.com>
2020-11-29 16:28:36 +0100

    Testing table view

--- src/main/java/com/example/jsonParser/JsonParserController.java
+++ src/main/java/com/example/jsonParser/JsonParserController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class JsonParserController {
 
@@ -16,7 +19,23 @@ public class JsonParserController {
   }
 
   @PostMapping("/table")
-  public String showTable(){
+  public String showTable(Model m){
+    List<String> headers = new ArrayList<>();
+    headers.add("#");
+    headers.add("Timestamp");
+    headers.add("Name");
+
+    List<String> row = new ArrayList<>();
+    row.add("1");
+    row.add("ts");
+    row.add("name1");
+
+    List<List<String>> rows = new ArrayList<>();
+    rows.add(row);
+
+    m.addAttribute("headers", headers);
+    m.addAttribute("rows", rows);
+
     return "table";
   }
 }
