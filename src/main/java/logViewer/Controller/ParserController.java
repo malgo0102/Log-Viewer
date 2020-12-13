65a171ae74ce42f1222543f5829a223593e54b93
Gheorghita <amaliacaldare20@gmail.com>
2020-12-13 17:48:01 +0100

    Search

--- src/main/java/logViewer/Controller/ParserController.java
+++ src/main/java/logViewer/Controller/ParserController.java
@@ -7,9 +7,7 @@ import logViewer.Parser.Parser;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -18,6 +16,7 @@ import java.util.List;
 @Controller
 public class ParserController {
 
+  TableData tableData;
   @PostMapping("/table")
   public String uploadFile(@RequestParam("file") MultipartFile multipartFile, Model model) {
 
@@ -27,12 +26,12 @@ public class ParserController {
     } else {
       try {
         //if (csv file):
-        Parser parser = new CsvParser();
+       Parser parser = new CsvParser();
         // if (json file):
-        //Parser parser = new JsonParser();
+      //  Parser parser = new JsonParser();
 
         String file = parser.readFile(multipartFile);
-        TableData tableData = parser.parse(file);
+        tableData = parser.parse(file);
         List<List<String>> rows = tableData.getRows();
         List<String> headers = tableData.getHeaders();
 
@@ -50,4 +49,21 @@ public class ParserController {
     return "table";
   }
 
+  @RequestMapping(value="/table")
+  public String search(@RequestParam("search") String search, Model model) {
+    List<List<String>> rows = tableData.getRows();
+    List<String> headers = tableData.getHeaders();
+    List<List<String>> filteredRows = new ArrayList<>();
+
+      for (List row : rows) {
+      if (row.contains(search)) {
+        filteredRows.add(row);
+      }
+    }
+
+      model.addAttribute("headers", headers);
+      model.addAttribute("rows", filteredRows);
+
+      return "table";
+    }
 }
