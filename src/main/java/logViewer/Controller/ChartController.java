76e20fa33f4bec7f50161855d70d035a62c13234
Amalia Caldare <amaliacaldare@Amalias-MacBook-Pro.local>
2020-12-23 19:36:04 +0100

    Removed un-used import

--- src/main/java/logViewer/Controller/ChartController.java
+++ src/main/java/logViewer/Controller/ChartController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 
 @Controller
