58c2b342644bdced5de421b803115dd15efb0cc6
Amalia Caldare <amaliacaldare@Amalias-MacBook-Pro.local>
2020-12-23 17:47:59 +0100

    Build query param

--- src/main/resources/static/chart.js
+++ src/main/resources/static/chart.js
@@ -2,6 +2,13 @@ $("#generate").click(function() {
           const urlX = $("#chooseX").val();
           const urlY = $("#chooseY").val();
             if (urlX !== "" && urlY !== ""){
-                window.location = "/chart" + urlX + urlY;
+                const obj = {
+                    x: urlX,
+                    y: urlY
+                }
+
+
+                alert($.param(obj));
+                window.location = "/chart?" + $.param(obj);
             }
 });
