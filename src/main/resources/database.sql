ef19204b2f141c45758c1132244b71cf14ef3b39
Malgo0102 <mal.wit@hotmail.com>
2021-01-17 14:54:45 +0100

    Regex corrected

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -2,7 +2,7 @@ DROP DATABASE log_viewer;
 CREATE DATABASE log_viewer;
 
 INSERT INTO file_format (id, file_type, name) VALUES ("1", "JSON", "My JSON setting");
-INSERT INTO file_format (id, file_type, name, regex) VALUES ("2", "CSV", "My CSV setting", "[ ,^-]+" );
+INSERT INTO file_format (id, file_type, name, regex) VALUES ("2", "CSV", "My CSV setting", "[ ,^]+" );
 
 INSERT INTO file_format_headers (file_format_id, headers) VALUES ("2", "IP address");
 INSERT INTO file_format_headers (file_format_id, headers) VALUES ("2", "Date");
